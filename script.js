50852161e78424c6d74b76bfff956db09b9fcc8f
Rishab-Rawat <rawatrishab66@gmail.com>
2024-07-17 19:13:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function fibonacci(num) {
-// your code here
+// your code her	
+	let fib=[0,1];
+	for(let i=2; i<=num; i++){
+		fib[i]=fib[i-1]+fib[i-2]
+	}
+	return fib[num-1]
 }
 
 module.exports = fibonacci;
